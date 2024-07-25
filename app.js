7c23496fe16cc541935e49a33bd591bdfa78f647
Carl Åberg <caaberg99@gmail.com>
2024-07-25 18:15:55 +0200

    adjusted ports in hopes of fixing deployment issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
